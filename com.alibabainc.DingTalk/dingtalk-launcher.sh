adcd679c46cefaff189891f920f45dacaf6f7d5c
gzzchh <xjdzch@126.com>
2022-02-25 01:55:14 +0800

    又修了几个库

--- com.alibabainc.DingTalk/dingtalk-launcher.sh
+++ com.alibabainc.DingTalk/dingtalk-launcher.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
-cd /opt/dingtalk-bin/*Release*/
+cd /app/extra/dingtalk/opt/apps/com.alibabainc.dingtalk/files/*Release*/
 LD_LIBRARY_PATH=$(pwd):$LD_LIBRARY_PATH ./com.alibabainc.dingtalk $1
