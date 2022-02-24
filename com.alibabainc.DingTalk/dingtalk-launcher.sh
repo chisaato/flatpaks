c7f758b889e72fe91e0618a669fb0b0852b4f1ea
chisaato <17796830+chisaato@users.noreply.github.com>
2022-02-25 02:23:26 +0800

    调整细节,描述性内容就等别人PR

--- com.alibabainc.DingTalk/dingtalk-launcher.sh
+++ com.alibabainc.DingTalk/dingtalk-launcher.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
-cd /app/extra/dingtalk/opt/apps/com.alibabainc.dingtalk/files/*Release*/
+cd /app/extra/dingtalk-bin
 LD_LIBRARY_PATH=$(pwd):$LD_LIBRARY_PATH ./com.alibabainc.dingtalk $1
