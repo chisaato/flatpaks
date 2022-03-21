54493531f6dd8307114d974fc9439c65e4c951fc
chisaato <17796830+chisaato@users.noreply.github.com>
2022-03-22 04:43:34 +0800

    来点CI 多好

---
+++ src/workflow.js
@@ -0,0 +1,37 @@
+const workflowMain = {
+	name: "构建镜像",
+	on: {
+		push: {
+			paths: [
+				// 放置扫描目录
+			],
+		},
+	},
+	jobs: {},
+};
+const buildJob = {
+	"runs-on": "ubuntu-latest",
+	name: "test-build-cloudmusic",
+	steps: [
+		{
+			name: "检出代码",
+			uses: "actions/checkout@v2",
+			with: {
+				submodules: "recursive",
+			},
+		},
+		{
+			name: "构建 Flatpak",
+			uses: "bilelmoussaoui/flatpak-github-actions/flatpak-builder@v4",
+			with: {
+				bundle: "com.netease.CloudMusic.flatpak",
+				"manifest-path": "com.netease.CloudMusic/com.netease.CloudMusic.yml",
+			},
+		},
+	],
+};
+
+module.exports = {
+	workflowMain: workflowMain,
+	buildJob: buildJob,
+};
