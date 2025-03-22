b75235cadcb317154212cbb855b1bd41d6aec24d
chisaato <17796830+chisaato@users.noreply.github.com>
2025-03-23 00:33:10 +0800

    feat: 更新 COSBrowser

--- src/workflow.js
+++ src/workflow.js
@@ -6,6 +6,7 @@ const workflowMain = {
 				// 放置扫描目录
 			],
 		},
+		workflow_dispatch: {},
 	},
 	jobs: {},
 };
@@ -13,7 +14,7 @@ const buildJob = {
 	"runs-on": "ubuntu-latest",
 	name: "test-build-cloudmusic",
 	container: {
-		image: "bilelmoussaoui/flatpak-github-actions:gnome-40",
+		image: "bilelmoussaoui/flatpak-github-actions:freesektop-24.08",
 		options: "--privileged",
 	},
 	steps: [
@@ -26,10 +27,18 @@ const buildJob = {
 		},
 		{
 			name: "构建 Flatpak",
-			uses: "bilelmoussaoui/flatpak-github-actions/flatpak-builder@v4",
+			uses: "flatpak/flatpak-github-actions/flatpak-builder@v4",
 			with: {
 				bundle: "com.netease.CloudMusic.flatpak",
 				"manifest-path": "com.netease.CloudMusic/com.netease.CloudMusic.yml",
+			},
+		},
+		{
+			name: "上传 Artifact",
+			uses: "actions/upload-artifact@v3",
+			with: {
+				name: "com.netease.CloudMusic.flatpak",
+				path: "com.netease.CloudMusic.flatpak",
 			},
 		},
 	],
